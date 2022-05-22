bddb5d79994af1f5c11af9a598ea47837ffed4d9
Kaze Wong <kazewong.physics@gmail.com>
2022-05-21 20:58:08 -0400

    Move binary to a separate class

--- projects/Backpop.py
+++ projects/Backpop.py
@@ -17,62 +17,61 @@ class DefaultTemplate(Scene):
 
 
 
-class OribitingBinary(ThreeDScene):
+class OrbitingBinary(ThreeDScene):
     def construct(self):
-        
         axes = ThreeDAxes()
         self.setup()
-        self.orbitAround()
+        self.add(Binary())
+        self.add(Binary().shift(3 * DOWN))
+        self.wait(10)
         
-        
-
     def setup(self):
-        self.orbit = Circle(radius=2).rotate(PI/8,axis=LEFT)
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
 
-    def orbitAround(self):
-        self.t_offset1 = 0.5
-        self.t_offset2 = 0.
+class Binary(VGroup):
+    def __init__(self):
+        super().__init__()
+        self.dot1 = Dot3D()
+        self.dot2 = Dot3D()
+        self.orbit = Circle(radius=2).rotate(PI/8,axis=LEFT)
+        self.dot1_dt = 0.
+        self.dot2_dt = 0.5
+        self.path1 = VMobject()
+
+        self.path2 = VMobject()
 
         def dot1_updater(mob, dt):
-            self.t_offset1 += dt * 0.1
-            self.t_offset1 = self.t_offset1 % 1
-            mob.move_to(self.orbit.point_from_proportion(self.t_offset1))
+            self.dot1_dt += dt * 0.1
+            self.dot1_dt = self.dot1_dt % 1
+            mob.move_to(self.orbit.point_from_proportion(self.dot1_dt))
 
         def dot2_updater(mob, dt):
-            self.t_offset2 += dt * 0.1
-            self.t_offset2 = self.t_offset2 % 1
-            mob.move_to(self.orbit.point_from_proportion(self.t_offset2))
+            self.dot2_dt += dt * 0.1
+            self.dot2_dt = self.dot2_dt % 1
+            mob.move_to(self.orbit.point_from_proportion(self.dot2_dt))
 
         def path1_updater(path):
             previous_path = path.copy()
-            previous_path.add_points_as_corners([dot1.get_center()])
+            previous_path.add_points_as_corners([self.dot1.get_center()])
             previous_path.points = previous_path.points[-50:]
             path.become(previous_path)
 
         def path2_updater(path):
             previous_path = path.copy()
-            previous_path.add_points_as_corners([dot2.get_center()])
+            previous_path.add_points_as_corners([self.dot2.get_center()])
             previous_path.points = previous_path.points[-50:]
             path.become(previous_path)
 
-        dot1 = Dot3D()
-        dot1.move_to(self.orbit.point_from_proportion(self.t_offset1))
-        dot1.add_updater(dot1_updater)
-        dot2 = Dot3D()
-        dot2.move_to(self.orbit.point_from_proportion(self.t_offset2))
-        dot2.add_updater(dot2_updater)
+        self.dot1.move_to(self.orbit.point_from_proportion(self.dot1_dt))
+        self.dot2.move_to(self.orbit.point_from_proportion(self.dot2_dt))
+        self.path1.set_points_as_corners([self.dot1.get_center(), self.dot1.get_center()])
+        self.path2.set_points_as_corners([self.dot2.get_center(), self.dot2.get_center()])
+        self.dot1.add_updater(dot1_updater)
+        self.dot2.add_updater(dot2_updater)
+        self.path1.add_updater(path1_updater)
+        self.path2.add_updater(path2_updater)
 
-        path1 = VMobject()
-        path1.set_points_as_corners([dot1.get_center(), dot1.get_center()])
-        path1.add_updater(path1_updater)
+        self.add(self.dot1,self.dot2,self.path1,self.path2)
+        
 
-        path2 = VMobject()
-        path2.set_points_as_corners([dot2.get_center(), dot2.get_center()])
-        path2.add_updater(path2_updater)
-
-        self.add(dot1)
-        self.add(dot2) 
-        self.add(path1)
-        self.add(path2)
-        self.wait(10)
+    
