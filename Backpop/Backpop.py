824c13c637c30493efe7d06b465bf6032a425dc1
Kaze Wong <kazewong.physics@gmail.com>
2022-05-27 21:14:00 -0400

    Add forward propagation

--- Backpop/Backpop.py
+++ Backpop/Backpop.py
@@ -1,3 +1,4 @@
+from cProfile import run
 from cmath import isnan
 from nturl2path import pathname2url
 from telnetlib import DO
@@ -11,16 +12,39 @@ class OrbitingBinary(ThreeDScene):
             output = -2*x+10
             return output
         self.distribution = Distribution(powerLaw)
-        self.binaryProcess = BinaryToRemnent()
-        self.add(self.distribution)
-        # self.play(Create(self.distribution))
-        # self.play(Create(self.binaryProcess))
+        binaryList = []
+        remnentList = []
+        arrowList = []
+        n_binary = 5
+        for i in range(n_binary):
+            binaryList.append(Binary(center = np.array([-2,i-2.5,0]), trail_length=1).set_color(interpolate_color(RED, BLUE, i/4)))
+            remnentList.append(Binary(radius=0, center = np.array([2,i-2.5,0]), trail_length=1).set_color(interpolate_color(RED, BLUE, i/4)))
+            arrowList.append(Arrow(binaryList[i].center, remnentList[i],buff=1).set_color(interpolate_color(RED, BLUE, i/4)))
 
-        # for i in np.linspace(0,1,1):
-        #     color_local = color.interpolate_color(color.RED,color.BLUE,i)
-        #     binary = Binary(radius = 0.2,trail_length=10).set_color(color_local)
-        #     self.add(binary)
-        # self.play(Rotate(binary),run_time=3)
+        # self.add(self.distribution)
+        self.play(Create(self.distribution))
+        self.wait(0.5)
+        self.play(Transform(self.distribution, self.distribution.copy().rotate(PI/2).scale(0.25).shift(3*LEFT)))
+        graph_points = self.distribution.graph.get_all_points()
+        self.play(*[GrowFromPoint(binaryList[i], graph_points[np.linspace(0,graph_points.shape[0]-1,n_binary).astype(int)][i]) for i in range(n_binary)])
+        for i in range(n_binary):
+            binaryList[i].trail_length = 60
+        # aniGroup = AnimationGroup(Rotate(binaryList[0]),lag_ratio=1)
+        self.play(AnimationGroup(*[Rotate(binaryList[i]) for i in range(n_binary)]),run_time=3)
+        for i in range(n_binary):
+            binaryList[i].trail_length = 1
+        
+        AnimationList = []
+        for i in range(n_binary-1,-1,-1):
+            AnimationList.append(FadeOut(arrowList[i],shift=RIGHT,scale=0))
+            AnimationList.append(Transform(binaryList[i],remnentList[i]))
+        self.play(AnimationGroup(*[GrowArrow(arrowList[i]) for i in range(n_binary-1,-1,-1)],lag_ratio=0.2))
+        self.play(AnimationGroup(*AnimationList,lag_ratio=0.2))
+        # self.play(AnimationGroup(*[FadeOut(arrowList[i],shift=RIGHT,scale=0) for i in range(n_binary-1,-1,-1)],lag_ratio=0.2),run_time=1)
+        # self.play(AnimationGroup(*[Transform(binaryList[i],remnentList[i]) for i in range(n_binary-1,-1,-1)],lag_ratio=0.2))
+
+
+
         
 
 class Binary(VGroup):
@@ -28,13 +52,13 @@ class Binary(VGroup):
         super().__init__()
         self.center = center
         self.trail_length = trail_length
-        self.dot1 = Dot3D().move_to(center)
-        self.dot2 = Dot3D().move_to(center)
+        self.dot1 = Dot().move_to(center)
+        self.dot2 = Dot().move_to(center)
         self.path1 = VMobject()
         self.path2 = VMobject()
 
         self.dot1.shift(radius*UP)
-        self.dot2.shift(radius*DOWN)
+        self.dot2.shift(radius*DOWN)            
 
         def path1_updater(path):
             previous_path = path.copy()
@@ -59,33 +83,24 @@ class Binary(VGroup):
     @override_animation(Rotate)
     def _rotate(self,**kwargs):
         dot1_rotate = Rotate(self.dot1,angle=6*PI,about_point=self.get_center(),rate_func=linear,)
-        dot2_rotate = Rotate(self.dot2,angle=6*PI,about_point=self.get_center(),rate_func=linear,)
+        dot2_rotate = Rotate(self.dot2,angle=6*PI,about_point=self.get_center(),rate_func=linear,)        
         return AnimationGroup(dot1_rotate,dot2_rotate)
 
+    
+
         
-class BinaryToRemnent(VGroup):
-    def __init__(self):
-        super().__init__()
-        self.binary_start = Binary(radius=0.2,center=np.array([0,0,0]),trail_length=0)
-        self.binary_end = Binary(radius=0.2,center=np.array([2,0,0]),trail_length=0)
-        self.arrow = Arrow(self.binary_start.get_center(),self.binary_end.get_center(),buff=0.1)
-
-    @override_animation(Create)
-    def _create(self,**kwargs):
-        return AnimationGroup(Create(self.binary_start), Rotate(self.binary_start), GrowArrow(self.arrow), Create(self.binary_end),lag_ratio=1)
-
 
 class Distribution(VGroup):
     def __init__(self, function):
         super().__init__()
         self.axes = Axes(x_range=[0,5], y_range=[0,10,10],axis_config={"include_tip": False})
-        y_label = self.axes.get_y_axis_label(r"p(m_{1})", edge=2*LEFT, direction=2*LEFT)
+        y_label = self.axes.get_y_axis_label(r"p(m_{1})", edge=LEFT, direction=LEFT).rotate(PI/2).shift(0.5*LEFT)
         x_label = self.axes.get_x_axis_label(r"m_{1}", edge=DOWN, direction=DOWN,buff=10)
-        grid_labels = VGroup(x_label, y_label)
+        self.grid_labels = VGroup(x_label, y_label)
         axis = np.linspace(0.1,5,100)
         self.graph = self.axes.plot_line_graph(x_values=axis,y_values=function(axis),line_color=RED,add_vertex_dots=False, stroke_width=4,)
-        self.add(self.axes,self.graph, grid_labels)
+        self.add(self.axes,self.graph, self.grid_labels)
 
     @override_animation(Create)
     def _create(self,**kwargs):
-        return AnimationGroup(Create(self.axes),Create(self.graph),lag_ratio=1)
+        return AnimationGroup(Create(self.axes),Create(self.graph),Create(self.grid_labels),lag_ratio=0.5)
