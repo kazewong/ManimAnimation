20652ea851e8f2fe1d531aaf9ead1c787a811b08
kazewong <kazewong.physics@gmail.com>
2022-11-02 23:54:25 -0400

    Animate group

--- FlowMC/FlowMC.py
+++ FlowMC/FlowMC.py
@@ -1,7 +1,6 @@
 from manim import *
-import jax.numpy as jnp
-import jax
-from jax.scipy.special import logsumexp
+import numpy as np
+from scipy.special import logsumexp
 
 class MonteCarloIntro(Scene):
     def construct(self):
@@ -31,18 +30,18 @@ def dualmoon(x):
     """
     Term 2 and 3 separate the distribution and smear it along the first and second dimension
     """
-    term1 = 0.5 * ((jnp.linalg.norm(x) - 2) / 0.1) ** 2
-    term2 = -0.5 * ((x[:1] - jnp.array([-5.0,5.0])) / 0.8) ** 2
+    term1 = 0.5 * ((np.linalg.norm(x) - 2) / 0.1) ** 2
+    term2 = -0.5 * ((x[:1] - np.array([-5.0,5.0])) / 0.8) ** 2
     return -(term1 - logsumexp(term2))
 
-dualmoon = jax.jit(dualmoon)
+# dualmoon = jax.jit(dualmoon)
 
 class TargetDistribution(Scene):
 
     def add_graph(self, level):
         graph = ImplicitFunction(
-            lambda x, y: dualmoon(jnp.array([x, y]))+level,
-            color=YELLOW,
+            lambda x, y: dualmoon(np.array([x, y]))+level,
+            color=BLUE,
             y_range=[-3,3],
             x_range=[-3,3],
         )
@@ -53,5 +52,6 @@ class TargetDistribution(Scene):
         level_set = [20, 15, 10, 5]
         for i in range(len(level_set)):
             graphs.append(self.add_graph(level_set[i]))
-            graphs[-1].set_opacity(0.1+0.2*i)
-        self.add(*graphs)
+            graphs[-1].set_opacity(0.2+0.2*i)
+        group = VGroup(*graphs)
+        self.play(AnimationGroup(*[FadeIn(graph) for graph in graphs], lag_ratio=0.5))
