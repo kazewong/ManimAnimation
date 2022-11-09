f79543f56660dad3659978622e5705fcbc8dd83f
kazewong <kazewong.physics@gmail.com>
2022-11-08 23:09:47 -0500

    Add FlowMC

--- FlowMC/FlowMC.py
+++ FlowMC/FlowMC.py
@@ -22,26 +22,6 @@ data_local = np.load('./dualmoon_data_local.npz')
 local_size = data_local['chains'].shape[1]
 global_size = data_global['chains'].shape[1]
 n_loop = 20
-chain_number = 5
-class TargetDistribution(Scene):
-
-    def add_graph(self, level):
-        graph = ImplicitFunction(
-            lambda x, y: dualmoon(np.array([x, y]))+level,
-            color=BLUE,
-            y_range=[-3,3],
-            x_range=[-3,3],
-        )
-        return graph
-
-    def construct(self):
-        graphs = []
-        level_set = [15, 10, 5, 0]
-        for i in range(len(level_set)):
-            graphs.append(self.add_graph(level_set[i]))
-            graphs[-1].set_opacity(0.2+0.2*i)
-            graphs[-1].set_stroke(width=0.)
-        self.play(AnimationGroup(*[FadeIn(graph) for graph in graphs], lag_ratio=0.5))
 
 class NormalMCMC(Scene):
 
@@ -55,6 +35,7 @@ class NormalMCMC(Scene):
         return graph
 
     def construct(self):
+        chain_number = 5
         graphs = []
         level_set = [15, 10, 5, 0]
         for i in range(len(level_set)):
@@ -63,12 +44,10 @@ class NormalMCMC(Scene):
             graphs[-1].set_stroke(width=0.)
         self.play(AnimationGroup(*[FadeIn(graph) for graph in graphs], lag_ratio=0.5))
 
-        # walker = Dot(np.array([data_local['chains'][chain_number,i,0], data_local['chains'][chain_number,i,1], 0]))
-        walker = Dot(np.array([data_global['chains'][chain_number,i,0], data_global['chains'][chain_number,i,1], 0]))
+        walker = Dot(np.array([data_local['chains'][chain_number,i,0], data_local['chains'][chain_number,i,1], 0]))
         self.play(FadeIn(walker))
         for i in range(1,n_loop):
-            # self.play(walker.animate.move_to(np.array([data_local['chains'][chain_number,::int(local_size/n_loop),0][i], data_local['chains'][chain_number,::int(local_size/n_loop),1][i], 0])))
-            self.play(walker.animate.move_to(np.array([data_global['chains'][chain_number,::int(global_size/n_loop),0][i], data_global['chains'][chain_number,::int(global_size/n_loop),1][i], 0])))
+            self.play(walker.animate.move_to(np.array([data_local['chains'][chain_number,::int(local_size/n_loop),0][i], data_local['chains'][chain_number,::int(local_size/n_loop),1][i], 0])))
 
 nf_data = np.load("./dualmoon_nf_samples.npz",allow_pickle=True)['nf_samples_array']
 class BuildingFlow(Scene):
@@ -113,6 +92,7 @@ class FlowMC(Scene):
         return graph
 
     def construct(self):
+        chain_number = 5
         graphs = []
         level_set = [15, 10, 5, 0]
         for i in range(len(level_set)):
@@ -121,9 +101,12 @@ class FlowMC(Scene):
             graphs[-1].set_stroke(width=0.)
         self.play(AnimationGroup(*[FadeIn(graph) for graph in graphs], lag_ratio=0.5))
 
-        # walker = Dot(np.array([data_local['chains'][chain_number,i,0], data_local['chains'][chain_number,i,1], 0]))
-        walker = Dot(np.array([data_global['chains'][chain_number,i,0], data_global['chains'][chain_number,i,1], 0]))
+        chain = data_global['chains'][chain_number]
+        steps_index = np.random.choice(np.where(chain[:,0]>0)[0],size=int(n_loop/4))
+        steps_index = np.append(steps_index,np.random.choice(np.where(chain[:,0]<0)[0],size=int(3*n_loop/4)))
+        np.random.shuffle(steps_index)
+        print(chain[steps_index[0]])
+        walker = Dot(np.array([chain[steps_index[0],0], chain[steps_index[0],1], 0]))
         self.play(FadeIn(walker))
-        for i in range(1,n_loop):
-            # self.play(walker.animate.move_to(np.array([data_local['chains'][chain_number,::int(local_size/n_loop),0][i], data_local['chains'][chain_number,::int(local_size/n_loop),1][i], 0])))
-            self.play(walker.animate.move_to(np.array([data_global['chains'][chain_number,::int(global_size/n_loop),0][i], data_global['chains'][chain_number,::int(global_size/n_loop),1][i], 0])))
+        for i in range(1,n_loop-1):
+            self.play(walker.animate.move_to(np.array([chain[steps_index[i],0], chain[steps_index[i],1], 0])))
