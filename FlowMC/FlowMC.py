0174763088f45157dc9fb3391b820ea43da07ca7
kazewong <kazewong.physics@gmail.com>
2022-11-09 01:50:20 -0500

    Finish NormalMCMC

--- FlowMC/FlowMC.py
+++ FlowMC/FlowMC.py
@@ -42,14 +42,21 @@ class NormalMCMC(Scene):
             graphs.append(self.add_graph(level_set[i]))
             graphs[-1].set_opacity(0.2+0.2*i)
             graphs[-1].set_stroke(width=0.)
+        text = Text("Normal MCMC", color=WHITE).shift(UP*3.5)
+        self.play(Write(text))
         self.play(AnimationGroup(*[FadeIn(graph) for graph in graphs], lag_ratio=0.5))
 
-        init_pos = np.array([data_local['chains'][chain_number,i,0], data_local['chains'][chain_number,i,1], 0])
+        
+        chain = data_local['chains'][chain_number,::int(local_size/n_loop)]
+
+        init_pos = np.array([chain[0,0], chain[0,1], 0])
+        final_pos = np.array([chain[-1,0], chain[-1,1], 0])
+
         walker = Dot(init_pos)
         proposal_Gaussian_inner = Circle(color=GREEN, radius=0.6, fill_opacity=0.4, stroke_width=0).move_to(init_pos)
         proposal_Gaussian_outer = Circle(color=GREEN, radius=1.0, fill_opacity=0.2, stroke_width=0).move_to(init_pos)
-        arrow = Arrow(init_pos, init_pos+0.2, color=YELLOW, stroke_width=0.5)
-
+        arrow = Arrow(init_pos, np.array([chain[1,0],chain[1,1],0]), color=YELLOW, max_stroke_width_to_length_ratio=5).scale(10,scale_tips=True)
+        red_arrow = Arrow(final_pos, np.array([chain[-1,0]-4.5, chain[-1,1],0]), color=WHITE, stroke_width=4)
 
         self.play(FadeIn(walker))
         self.wait(0.5)
@@ -58,14 +65,17 @@ class NormalMCMC(Scene):
 
         self.play(GrowArrow(arrow))
         self.wait(0.5)
-        self.play(Transform(arrow, Arrow(init_pos, init_pos, color=YELLOW, stroke_width=0.5)))
+        self.play(Transform(arrow, Arrow(init_pos, init_pos, color=YELLOW, stroke_width=0)))
         self.wait(0.5)
 
         self.play(FadeOut(proposal_Gaussian_inner), FadeOut(proposal_Gaussian_outer))
         self.wait(0.5)
 
         for i in range(1,n_loop):
-            self.play(walker.animate.move_to(np.array([data_local['chains'][chain_number,::int(local_size/n_loop),0][i], data_local['chains'][chain_number,::int(local_size/n_loop),1][i], 0])))
+            self.play(walker.animate.move_to(np.array([chain[i][0], chain[i][1], 0])))
+
+        self.play(GrowArrow(red_arrow))
+        self.play(Indicate(red_arrow, color=RED,scale_factor=1))
 
 nf_data = np.load("./dualmoon_nf_samples.npz",allow_pickle=True)['nf_samples_array']
 class BuildingFlow(Scene):
