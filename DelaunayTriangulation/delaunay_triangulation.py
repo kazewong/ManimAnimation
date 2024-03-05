ae66eab721d94c650a96cfea932d6026b2a059f6
kazewong <kazewong.physics@gmail.com>
2024-03-05 17:33:14 -0500

    Add Simplex class and update scene for initial construction and one insertion

--- DelaunayTriangulation/delaunay_triangulation.py
+++ DelaunayTriangulation/delaunay_triangulation.py
@@ -21,18 +21,35 @@ def plot_simplex_2d(simplex: dict[int, np.array], vertices: list[float]):
 
 simplices, vertices = read_data('/home/kaze/Work/Presentation/ManimAnimation/DelaunayTriangulation/tree.npz')
 
+class Simplex(VGroup):
+    def __init__(self, vertices, **kwargs):
+        super().__init__(**kwargs)
+        self.dot_group = VGroup()
+        for vertex in vertices:
+            self.dot_group.add(Dot(np.array([vertex[0], vertex[1], 0])*2))
+        self.line_group = VGroup()
+        for i in range(3):
+            self.line_group.add(Line(self.dot_group[i].get_center(), self.dot_group[(i+1)%3].get_center()))
+        self.add(self.dot_group, self.line_group)
+        
+    @override_animation(Create)
+    def _create(self,**kwargs):
+        anigroup1 = AnimationGroup(*[Create(dot) for dot in self.dot_group])
+        anigroup2 = AnimationGroup(*[Create(line) for line in self.line_group])
+        return AnimationGroup(anigroup1, anigroup2, lag_ratio=0.85)
+
 class InitialConstruction(Scene):
     def construct(self):
-        dot_group = VGroup()
-        for vertex in vertices[:3]:
-            dot_group.add(Dot(np.array([vertex[0], vertex[1], 0])*2))
-        animation_group = AnimationGroup(*[Create(Dot(np.array([vertex[0], vertex[1], 0])*2)) for vertex in vertices[:3]])
-        self.play(animation_group)
-        line_group = VGroup()
-        for i in range(3):
-            line_group.add(Line(dot_group[i].get_center(), dot_group[(i+1)%3].get_center()))
-        animation_group = AnimationGroup(*[Create(line) for line in line_group])
-        self.play(animation_group)
+        simplex = Simplex(vertices[:3])
+        self.play(Create(simplex))
+
+class OneInsertion(Scene):
+    def construct(self):
+        initial_simplex = Simplex(vertices[:3])
+        self.add(initial_simplex)
+
+
+
 class ConstructDelanuayTriangulation(Scene):
     def construct(self):
         #Insert vertices
