c246490770291898c88a869711640bc986d2731c
Kaze Wong <kazewong.physics@gmail.com>
2024-03-06 06:15:47 -0500

    Refactor Simplex class to allow customizable dot and line colors

--- DelaunayTriangulation/delaunay_triangulation.py
+++ DelaunayTriangulation/delaunay_triangulation.py
@@ -30,21 +30,45 @@ for i in range(3, 20):
     vertices.append(_vertices)
 
 class Simplex(VGroup):
-    def __init__(self, vertices, **kwargs):
+    def __init__(self, vertices, dot_color:list[str] | str = WHITE, line_color:list[str] | str = WHITE, **kwargs):
         super().__init__(**kwargs)
         self.dot_group = VGroup()
         for vertex in vertices:
-            self.dot_group.add(Dot(np.array([vertex[0], vertex[1], 0])*2))
+            if isinstance(dot_color, list):
+                color = dot_color.pop(0)
+            else:
+                color = dot_color
+            self.dot_group.add(Dot(np.array([vertex[0], vertex[1], 0])*2,color=color))
         self.line_group = VGroup()
         for i in range(3):
-            self.line_group.add(Line(self.dot_group[i].get_center(), self.dot_group[(i+1)%3].get_center()))
+            if isinstance(line_color, list):
+                color = line_color.pop(0)
+            else:
+                color = line_color
+            self.line_group.add(Line(self.dot_group[i].get_center(), self.dot_group[(i+1)%3].get_center(), color=color))
         self.add(self.dot_group, self.line_group)
         
     @override_animation(Create)
-    def _create(self,**kwargs):
-        anigroup1 = AnimationGroup(*[Create(dot) for dot in self.dot_group])
-        anigroup2 = AnimationGroup(*[Create(line) for line in self.line_group])
-        return AnimationGroup(anigroup1, anigroup2, lag_ratio=0.85)
+    def _create(self,dot=True, line=True,**kwargs):
+        output = []
+        if dot:
+            output.append(AnimationGroup(*[Create(dot) for dot in self.dot_group]))
+        if line:
+            output.append(AnimationGroup(*[Create(line) for line in self.line_group]))
+        return AnimationGroup(*output, lag_ratio=0.85)
+
+    @override_animation(Uncreate)
+    def _uncreate(self, dot=True, line=True, **kwargs):
+        output = []
+        if dot:
+            output.append(AnimationGroup(*[Uncreate(dot) for dot in self.dot_group]))
+        if line:
+            output.append(AnimationGroup(*[Uncreate(line) for line in self.line_group]))
+        return AnimationGroup(*output, lag_ratio=0.85)
+
+    @override_animation(Indicate)
+    def _indicate(self, **kwargs):
+        return Indicate(self.dot_group, **kwargs)
 
 class InitialConstruction(Scene):
     def construct(self):
@@ -53,16 +77,21 @@ class InitialConstruction(Scene):
 
 class OneInsertion(Scene):
     def construct(self):
-        new_vertex = Dot(np.array([vertices[0][6][0], vertices[0][6][1], 0]))
+        simplex = Simplex(vertices[0][:3])
+        self.add(simplex)
+
+        new_vertex = Dot(np.array([vertices[0][6][0], vertices[0][6][1], 0])*2, z_index=100)
+
         new_simplex_group = VGroup()
-        print(simplices[0].values())
-        
         simplex_with_vertex = list(filter(lambda simplex: 7 in simplex[0], simplices[0].values()))
-        print(simplex_with_vertex)
+        color_list = [RED, GREEN, BLUE]
         for i in range(3):
-            new_simplex_group.add(Simplex(vertices[0][simplex_with_vertex[i][0]-1]))
-        self.play(Create(new_vertex))
-        animation_group = AnimationGroup(*[Create(simplex) for simplex in new_simplex_group])
+            new_simplex_group.add(Simplex(vertices[0][simplex_with_vertex[i][0]-1], dot_color=color_list[i], line_color=color_list[i]))
+
+        self.play(Create(new_vertex))        
+        # self.play(Create(new_simplex_group[0],line=False))
+        self.play(Indicate(simplex, scale_factor=1, color=RED))
+        animation_group = AnimationGroup(*[Create(simplex,dot=False) for simplex in new_simplex_group])
         self.play(animation_group)
         
 class ConstructDelanuayTriangulation(Scene):
